6310228524320558c430fae3a291d3787cf5b168
= <=>
2021-03-05 17:06:45 -0600

    making changes to plots3d function prep script

--- +customFunc/+visual/Plots3D.m
+++ +customFunc/+visual/Plots3D.m
@@ -1,41 +1,43 @@
-origin = [0 0 0];
+clear;clc;clf;
 
-% Create initial vectors
-xyz = [1,0,0]';
-xyz2 = [1,0,0]';
+oldFrame = eye(4);
+newFrame = oldFrame;
 
-% Euler
-xyz = customFunc.rotate.rotate_Zd(90)*xyz;
-xyz = customFunc.rotate.rotate_Xd(45)*xyz;
-
-x = [0,xyz(1)]; 
-y = [0,xyz(2)]; 
-z = [0,xyz(3)];
-
-x2 = [0,xyz2(1)]; 
-y2 = [0,xyz2(2)]; 
-z2 = [0,xyz2(3)];
+newFrame = customFunc.links.Link2Transform(0,4,3,90)*newFrame;
 
 %Make Axis Lines
 figure1 = figure(1);
-axis = [-1,1];
+axis = [-600,600];
 zero = [0,0];
 plot3(axis,zero,zero,'--k');
 hold on;
 plot3(zero,axis,zero,'--k');
 plot3(zero,zero,axis,'--k');
 
-%Plot Origin
+%Plot oldFrame
 ax = gca(figure1);
 
-delta = 0.5;
-line('XData', [origin(1) origin(1) + delta], 'YData', [origin(2) origin(2)],...
-    'ZData', [origin(3) origin(3)], 'Color','r');
-line('XData', [origin(1) origin(1)], 'YData', [origin(2) origin(2) + delta],...
-    'ZData', [origin(3) origin(3)], 'Color','g');
-line('XData', [origin(1) origin(1)], 'YData', [origin(2) origin(2)],...
-    'ZData', [origin(3) origin(3) + delta], 'Color','b');
+% Generate origin plot properties
+x_old = oldFrame(1:3,4)*oldFrame(1,1:3);
+y_old = oldFrame(1:3,4)*oldFrame(1,1:3);
+z_old = oldFrame(1:3,4)*oldFrame(1,1:3);
 
-% xlim([-1,1]); xlabel('X Axis');
-% ylim([-1,1]); ylabel('Y Axis');
-% zlim([-1,1]); zlabel('Z Axis');
+% Graph Origin Coordinate Frame
+
+
+% delta = 0.5;
+% line('XData', [oldFrame(1) oldFrame(1) + delta], 'YData', [oldFrame(2) oldFrame(2)],...
+%     'ZData', [oldFrame(3) oldFrame(3)], 'Color','r','LineWidth',0.8);
+% line('XData', [oldFrame(1) oldFrame(1)], 'YData', [oldFrame(2) oldFrame(2) + delta],...
+%     'ZData', [oldFrame(3) oldFrame(3)], 'Color','g','LineWidth',0.8);
+% line('XData', [oldFrame(1) oldFrame(1)], 'YData', [oldFrame(2) oldFrame(2)],...
+%     'ZData', [oldFrame(3) oldFrame(3) + delta], 'Color','b','LineWidth',0.8);
+% 
+% line('XData', [newFrame(1) newFrame(1) + delta], 'YData', [newFrame(2) newFrame(2)],...
+%     'ZData', [newFrame(3) newFrame(3)], 'Color','r','LineWidth',0.8);
+% line('XData', [newFrame(1) newFrame(1)], 'YData', [newFrame(2) newFrame(2) + delta],...
+%     'ZData', [newFrame(3) newFrame(3)], 'Color','g','LineWidth',0.8);
+% line('XData', [newFrame(1) newFrame(1)], 'YData', [newFrame(2) newFrame(2)],...
+%     'ZData', [newFrame(3) newFrame(3) + delta], 'Color','b','LineWidth',0.8);
+
+xlim([
